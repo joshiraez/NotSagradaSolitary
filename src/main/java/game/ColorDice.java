fce2dd283f097a96f1089e707115bbd13f85c8e9
JoshiRaez <joseraezrodriguez@gmail.com>
2020-02-23 19:40:27 +0100

    ColorDice was not committed (?????)

---
+++ src/main/java/game/ColorDice.java
@@ -0,0 +1,14 @@
+package game;
+
+class ColorDice {
+
+    final private DiceColor color;
+
+    ColorDice(DiceColor color) {
+        this.color = color;
+    }
+
+    DiceColor getColor() {
+        return color;
+    }
+}
